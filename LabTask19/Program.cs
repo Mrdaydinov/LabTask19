b64586f9e839d302f9d2bd389d8551bf13128d11
Murad <mrdaydinov@gmail.com>
2024-04-03 13:33:25 +0400

    init commit

---
+++ LabTask19/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection.Emit;
+
+namespace LabTask19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        label:
+            int input = int.Parse(Console.ReadLine());
+
+            if (input < 999)
+            {
+                Console.WriteLine("need 4+ digs");
+                goto label;
+            }
+
+
+            int firstDig = input;
+            int count = 0;
+
+            
+            while (firstDig > 0)
+            {
+                firstDig /= 10;
+                count++;
+            }
+
+
+            int[] digs = new int[count];
+
+            firstDig = input;
+            int lastDig = 0;
+            for (int i = 0; i < digs.Length; i++)
+            {
+                lastDig = firstDig % 10;
+                digs[i] = lastDig;
+                firstDig /= 10;
+            }
+
+            for (int i = 0; i < digs.Length; i++)
+            {
+                Console.WriteLine(digs[i]);
+            }
+        }
+    }
+}
